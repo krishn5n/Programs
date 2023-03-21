8b1bd0a56f74f16f94cf21aa7479f12571285a25
krishn5n <128227197+krishn5n@users.noreply.github.com>
2023-03-21 22:25:00 +0530

    Update transpose_of_square_matrix.c

--- transpose_of_square_matrix.c
+++ transpose_of_square_matrix.c
@@ -1,26 +1,41 @@
-//Find rightmost non zero integer of a factorial
+//Finding transpose of a square matrix
 #include <stdio.h>
 int main(){
-    //Get number for which to calculate factorial
-    int n;
-    scanf("%d",&n);
-    int i;
-    int fact=1;
-    //finding facftorial
-    for(i=1; i<=n; i++){
-        fact=fact*i;
-    }
+    // Getting input of square matrix from user
+    int row,column;
+    scanf("%d",&row);
+    scanf("%d",&column);
+    int matrix[row][column];
+    int i,j;
     int t=1;
-    //finding last most digit
-    while (t==1){
-        //checking if last most digit is 0 or not
-        if (fact%10==0){
-            fact=(fact/10);
-        }
-        else{
-            printf("The rightmost non zero integer of factorial of %d is %d",n,fact%10);
+    // Creating the matrix by taking values as input from user
+    for (i=0; i<row; i++){
+        for (j=0; j<column ; j++){
+            int number;
+            scanf("%d",&number);
+            matrix[i][j]=number;
+            printf("digits added = %d\n",t);
             t=t+1;
         }
+    }
+    //transpose a matrix
+    int k,l;
+    for (k=0; k<row; k++){
+        for (l=k; l<column; l++){
+            int x;
+            x=matrix[k][l];
+            matrix[k][l]=matrix[l][k];
+            matrix[l][k]=x;
+        }
+    }
+    // printing matrix's transpose
+    printf("Matrix transpose is\n");
+    int m,n;
+    for (m=0 ; m<row ; m++){
+        for (n=0; n<column; n++){
+            printf("%d  ",matrix[m][n]);
+        }
+        printf("\n");
     }
     return 0;
 }
